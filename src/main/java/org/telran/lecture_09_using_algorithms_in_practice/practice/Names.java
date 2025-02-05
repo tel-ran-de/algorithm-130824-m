6ad5fbfa5bd1a4c57c394cd92307820d031bb6f2
mshchuro <150063341+mshchuro@users.noreply.github.com>
2025-02-05 13:05:12 +0200

    Update Names.java
    Changes for task Names from Maksims Ščurovskis

--- src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Names.java
+++ src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Names.java
@@ -4,4 +4,23 @@ package org.telran.lecture_09_using_algorithms_in_practice.practice;
 // Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
 public class Names {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList(
+                "Max",
+                "Evnegiy",
+                "Anastasiya"
+        );
+
+        String longestName = "";
+
+        for (String n : names) {
+            if (n.length() > longestName.length()) {
+                longestName = n;
+            }
+        }
+
+        System.out.println("Longest name is - " + longestName);
+    }
 }

d2883ffa1b73498f19b577b2a8ff616ef7c4a3a2
mshchuro <150063341+mshchuro@users.noreply.github.com>
2025-02-05 13:30:47 +0200

    Update Anagram.java
    Changes from Maksims Ščurovskis

--- src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Anagram.java
+++ src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Anagram.java
@@ -4,4 +4,32 @@ package org.telran.lecture_09_using_algorithms_in_practice.practice;
 // Проверить, являются ли они анаграммами (состоят из одних и тех же букв, но в разном порядке).
 
 public class Anagram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first word please");
+        String firstWord = scanner.next();
+
+        System.out.println("Enter second word please");
+        String secondWord = scanner.next();
+
+        firstWord = firstWord.toLowerCase();
+        secondWord = secondWord.toLowerCase();
+
+        char[] word1 = firstWord.toCharArray();
+        char[] word2 = secondWord.toCharArray();
+
+        Arrays.sort(word1);
+        Arrays.sort(word2);
+
+        System.out.println(word1);
+        System.out.println(word2);
+
+        if (Arrays.equals(word1, word2)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
 }

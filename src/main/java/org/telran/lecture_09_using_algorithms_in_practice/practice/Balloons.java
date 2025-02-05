93925ed2276c4ccc72a58bc35aa07cd07d099eab
SerhiiKurlykin <kurlykin83@gmail.com>
2025-02-05 12:25:46 +0100

    Update Balloons.java

--- src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Balloons.java
+++ src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Balloons.java
@@ -19,4 +19,25 @@ package org.telran.lecture_09_using_algorithms_in_practice.practice;
 // Пояснение: Никакими комбинациями 5 и 3, 7 шариков купить невозможно
 
 public class Balloons {
+
+    public static void main(String[] args) {
+        int k = 7;
+
+        if (canBuy(k)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean canBuy(int k) {
+        for (int i = 0; i <= k / 3; i++) {
+            for (int j = 0; j <= k / 5; j++) {
+                if (i * 3 + j * 5 == k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }

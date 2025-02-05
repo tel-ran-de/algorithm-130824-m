c181cb04725ca8bb80ae31ff30d3e3e8d4e7ce01
AntonSchellhorn <Jackpot.8.8.8.8.8A@gmail.com>
2025-02-05 12:26:08 +0100

    Update Anagram.java
    The first is went

--- src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Anagram.java
+++ src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Anagram.java
@@ -3,5 +3,28 @@ package org.telran.lecture_09_using_algorithms_in_practice.practice;
 // Даны две строки.
 // Проверить, являются ли они анаграммами (состоят из одних и тех же букв, но в разном порядке).
 
+package org.telran.lecture_09_using_algorithms_in_practice.practice;
+
+// Даны две строки.
+// Проверить, являются ли они анаграммами (состоят из одних и тех же букв, но в разном порядке).
+
+import java.util.Arrays;
+
 public class Anagram {
+    public static String wordOne = "Cod";
+    public static String wordTwo = "Doc";
+
+    public static void main(String[] args) {
+        char[] one = wordOne.toLowerCase().toCharArray();
+        char[] two = wordTwo.toLowerCase().toCharArray();
+
+        Arrays.sort(one);
+        Arrays.sort(two);
+
+        if (Arrays.equals(one,two)){
+            System.out.println("Строки являются анаграммами");
+        }else {
+            System.out.println("Ошибочка вышла");
+        }
+    }
 }

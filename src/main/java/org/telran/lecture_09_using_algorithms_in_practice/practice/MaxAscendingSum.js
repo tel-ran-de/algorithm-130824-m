bc56f7980d60b6e8d346c744f122056adaaa8522
KaterynaKs <131392942+KaterynaKs@users.noreply.github.com>
2025-02-12 00:14:58 +0100

    Update MaxAscendingSum.js

--- src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/MaxAscendingSum.js
+++ src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/MaxAscendingSum.js
@@ -13,3 +13,29 @@
 // Input: nums = [10,20,30,40,50]
 // Output: 150
 // Пояснение: [10,20,30,40,50] — возрастающий подмассив с максимальной суммой 65.
+
+const nums = [10, 20, 25, 67, 5, 4, 70, 89]
+
+if (nums.length === 0) {
+    console.log(0);  
+} else {
+    let maxSum = nums[0]; 
+    let currentSum = nums[0];  
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {  
+            currentSum += nums[i];  
+        } else {
+            if (currentSum > maxSum) {  
+                maxSum = currentSum;  
+            }
+            currentSum = nums[i];  
+        }
+    }
+
+    if (currentSum > maxSum) {
+        maxSum = currentSum;
+    }
+
+    console.log(maxSum);  
+}

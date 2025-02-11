54c29e813396a5301f3ecf4481f792c5e3d31c81
Julia Frolova <jmabracodabra@gmail.com>
2025-02-11 21:27:51 +0100

    Update Anagram.java

--- src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Anagram.java
+++ src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Anagram.java
@@ -4,4 +4,28 @@ package org.telran.lecture_09_using_algorithms_in_practice.practice;
 // Проверить, являются ли они анаграммами (состоят из одних и тех же букв, но в разном порядке).
 
 public class Anagram {
+  
+  public static void main(String[] args) {
+        String strOne = "q1w2e13";
+        String strTwo = "1q2w3e1";
+        System.out.println(isAnagram(strOne, strTwo));
+    }
+
+    private static boolean isAnagram(String strOne, String strTwo) {
+        if (strOne.length() != strTwo.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> characters = new HashMap<>();
+
+        for (Character c : strOne.toCharArray()) {
+            characters.put(c, characters.getOrDefault(c, 0) + 1);
+        }
+
+        for (Character c : strTwo.toCharArray()) {
+            characters.put(c, characters.getOrDefault(c, 0) - 1);
+        }
+
+        return characters.values().stream().allMatch(c -> c == 0);
+    }
 }

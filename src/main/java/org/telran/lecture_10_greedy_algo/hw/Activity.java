a68c3662da3e1d4f30ff46ac414f81be159dc545
boo <booblegum42@gmail.com>
2025-02-19 18:02:14 +0545

    HW to Lecture_10

--- src/main/java/org/telran/lecture_10_greedy_algo/hw/Activity.java
+++ src/main/java/org/telran/lecture_10_greedy_algo/hw/Activity.java
@@ -13,9 +13,44 @@ package org.telran.lecture_10_greedy_algo.hw;
 //  Необходимо использовать жадный алгоритм.
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class ActivityItem {
+    public String name;
+    public int start;
+    public int end;
+    public int cost;
+
+    public ActivityItem(String name, int start, int end, int cost) {
+        this.name = name;
+        this.start = start;
+        this.end = end;
+        this.cost = cost;
+    }
+}
+
 public class Activity {
-    private String name;
-    private int start;
-    private int end;
-    private int cost;
+    public static void main(String[] args) {
+        ActivityItem[] activities = {
+                new ActivityItem("drawing", 10, 13, 60),
+                new ActivityItem("writing", 10, 17, 40),
+                new ActivityItem("eating", 12, 14, 40),
+                new ActivityItem("coding", 15, 19, 40),
+                new ActivityItem("reading", 13, 16, 100),
+                new ActivityItem("dancing", 17, 18, 20)
+        };
+
+        Arrays.sort(activities, Comparator.comparingInt(
+                (ActivityItem a) -> -a.cost)
+                .thenComparingInt((ActivityItem a) -> a.end)
+        );
+        print(activities);
+    }
+    public static void print(ActivityItem[] activities) {
+        for (ActivityItem activity : activities) {
+            System.out.println("name: " + activity.name + " start: " + activity.start + " end: " + activity.end + " cost: " + activity.cost);
+        }
+    }
 }

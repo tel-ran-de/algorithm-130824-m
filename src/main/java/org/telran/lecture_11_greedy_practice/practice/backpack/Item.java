ea1dfdc49112ab425fda252aef74355d398b02bb
SerhiiKurlykin <kurlykin83@gmail.com>
2025-02-19 12:08:41 +0100

    Update Item.java

--- src/main/java/org/telran/lecture_11_greedy_practice/practice/backpack/Item.java
+++ src/main/java/org/telran/lecture_11_greedy_practice/practice/backpack/Item.java
@@ -5,4 +5,21 @@ public class Item {
     public int weight;
     public int value;
     public int unit_value; // Удельная ценность - цена за единицу веса
+
+    public Item(String name, int weight, int value) {
+        this.name = name;
+        this.weight = weight;
+        this.value = value;
+        this.unit_value = (int) value / weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                ", value=" + value +
+                ", unit_value=" + unit_value +
+                '}';
+    }
 }

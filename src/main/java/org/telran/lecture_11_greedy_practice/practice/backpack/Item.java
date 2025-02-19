244c393b91a53f5423b90a84f77f742d7f1cff3a
Roman Dogoda <piterom911@gmail.com>
2025-02-19 12:12:43 +0100

    task Item done

--- src/main/java/org/telran/lecture_11_greedy_practice/practice/backpack/Item.java
+++ src/main/java/org/telran/lecture_11_greedy_practice/practice/backpack/Item.java
@@ -1,8 +1,28 @@
 package org.telran.lecture_11_greedy_practice.practice.backpack;
 
+import lombok.Getter;
+
+@Getter
 public class Item {
-    public String name;
-    public int weight;
-    public int value;
-    public int unit_value; // Удельная ценность - цена за единицу веса
+    private String name;
+    private int weight;
+    private int value;
+    private int unit_value; // Удельная ценность - цена за единицу веса
+
+    public Item(String name, int weight, int value) {
+        this.name = name;
+        this.weight = weight;
+        this.value = value;
+        this.unit_value = value / weight;
+    }
+
+    @Override
+    public String toString() {
+        return "\nItem{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                ", value=" + value +
+                ", unit_value=" + unit_value +
+                "}";
+    }
 }

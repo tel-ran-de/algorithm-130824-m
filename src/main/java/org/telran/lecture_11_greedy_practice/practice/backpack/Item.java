7684909b0fdac05eb634c3d9ee9ddcb4f5d6fdc3
boo <booblegum42@gmail.com>
2025-02-19 13:51:10 +0545

    add new task to lecture-11

---
+++ src/main/java/org/telran/lecture_11_greedy_practice/practice/backpack/Item.java
@@ -0,0 +1,8 @@
+package org.telran.lecture_11_greedy_practice.practice.backpack;
+
+public class Item {
+    public String name;
+    public int weight;
+    public int value;
+    public int unit_value; // Удельная ценность - цена за единицу веса
+}

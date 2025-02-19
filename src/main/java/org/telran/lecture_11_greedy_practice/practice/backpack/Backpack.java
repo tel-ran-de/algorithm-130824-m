48a1be12d63c4ffd0b3049ff043f0bd7a97a30ff
Kostiantyn19899 <sierikov.de@gmail.com>
2025-02-19 12:04:51 +0100

    Update Backpack.java

--- src/main/java/org/telran/lecture_11_greedy_practice/practice/backpack/Backpack.java
+++ src/main/java/org/telran/lecture_11_greedy_practice/practice/backpack/Backpack.java
@@ -1,20 +1,23 @@
 package org.telran.lecture_11_greedy_practice.practice.backpack;
 
-// Есть набор предметов, у каждого предмета есть: цена и вес.
-// Требуется выбрать из заданного набора предметов наиболее ценные, которые поместятся в рюкзак заданной вместимости.
-
-// Алгоритм
-// 1. Рассчитываем удельную ценность для каждого предмета.
-// 2. Сортируем предметы по убыванию удельной ценности.
-// 3. Складываем предметы в рюкзак(массив), по суммарный вес не превысит максимально допустимый
-
-public class Backpack {
-    public static void main(String[] args) {
         Item[] items = new Item[]{ // Исходный набор предметов
-                new Item(),
-                new Item(),
+                new Item("aple", 2, 50, 25),
+                new Item("banana", 3, 30, 10),
+                new Item("pine", 5, 25, 5),
+                new Item("orange", 7, 14, 2),
+                new Item("kivi", 5, 200, 40),
         };
-        int totalWeight = 10; // Максимальная вместимость рюкзака
-    }
+        int totalWeight = 0; // Максимальная вместимость рюкзака
 
-}
+        Arrays.sort(items, Comparator.comparing(Item::getUnit_value).reversed());
+        System.out.println(Arrays.toString(items));
+        List<Item> listRes = new ArrayList<>();
+        for (Item item : items) {
+            if (totalWeight < 10) {
+                listRes.add(item);
+                totalWeight = totalWeight + item.getWeight();
+            }
+        }
+        System.out.println("----------------------------");
+        System.out.println(listRes);
+    }

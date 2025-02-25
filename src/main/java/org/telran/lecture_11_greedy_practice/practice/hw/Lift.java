508cf66b0c1055d2674d0909af5162021b037f42
Julia Frolova <jmabracodabra@gmail.com>
2025-02-25 19:04:24 +0100

    Added solution for task Lift

--- src/main/java/org/telran/lecture_11_greedy_practice/practice/hw/Lift.java
+++ src/main/java/org/telran/lecture_11_greedy_practice/practice/hw/Lift.java
@@ -1,20 +1,33 @@
 package org.telran.lecture_11_greedy_practice.practice.hw;
 
-// Задача: вызов лифта
-// В доме n лифтов, у каждого лифта своя скорость, измеряемая в сек/этаж (секунд на один этаж).
-// Реализовать алгоритм отправки лифта, который приедет быстрее всего.
-// Алгоритм будет применяться в домах разной этажности.
-// Алгоритм будет применяться в домах с количеством > 2 лифтов
-// Пример:
-//  в доме 19 этажей
-//  работают лифт А, лифт В и лифт C
-//  лифт А находится на 4 этаже и движется со скоростью 3 сек/этаж.
-//  лифт В находится на 8 этаже и движется со скоростью 2 сек/этаж.
-//  лифт C находится на 5 этаже и движется со скоростью 2 сек/этаж.
-//  На вход: 1 (вызываем на первый этаж)
-//  результат: отправляем лифт С
-
-
-
 public class Lift {
+
+    private String name;
+
+    private double speed;
+
+    private int currentFloor;
+
+    public Lift(String name, double speed, int floor) {
+        this.name = name;
+        this.speed = speed;
+        this.currentFloor = floor;
+    }
+
+    public double diffBetweenFloors(int targetFloor) {
+        return Math.abs(targetFloor - currentFloor);
+    }
+
+    public double timeToReach(int targetFloor) {
+        return diffBetweenFloors(targetFloor) * speed;
+    }
+
+    @Override
+    public String toString() {
+        return "Lift{" +
+                "name='" + name + '\'' +
+                ", speed=" + speed +
+                ", floor=" + currentFloor +
+                '}';
+    }
 }
